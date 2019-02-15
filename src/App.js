4ef753dc719b5ab7554c434547c88bd84947b433
cuong.duong <cuong.duong@titancorpvn.com>
2019-02-15 11:25:40 +0700

    link to other pages in navigation header/drawer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import { Link } from 'react-router-dom';
+
+import Main from './components/main';
+
 
 class App extends Component {
   render() {
@@ -10,22 +13,25 @@ class App extends Component {
         <Layout>
           <Header title="Title" scroll>
             <Navigation>
-              <a href="#">Link</a>
-              <a href="#">Link</a>
-              <a href="#">Link</a>
-              <a href="#">Link</a>
+              <Link to="/">Home</Link>
+              <Link to="/resume">Resume</Link>
+              <Link to="/aboutme">About Me</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/contact">Contact</Link>
             </Navigation>
           </Header>
           <Drawer title="Title">
             <Navigation>
-              <a href="#">Link</a>
-              <a href="#">Link</a>
-              <a href="#">Link</a>
-              <a href="#">Link</a>
+              <Link to="/">Home</Link>
+              <Link to="/resume">Resume</Link>
+              <Link to="/aboutme">About Me</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/contact">Contact</Link>
             </Navigation>
           </Drawer>
           <Content>
             <div className="page-content" />
+            <Main />
           </Content>
         </Layout>
       </div>
